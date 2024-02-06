4c71e194b275f2191021592d453892da472fdf60
Paúl Jheferson Guevara Linares <pguevarl@emeal.nttdata.com>
2024-02-06 17:38:14 -0500

    initial commit

---
+++ src/main/java/com/example/demopjgl/DemoPjglApplication.java
@@ -0,0 +1,15 @@
+package com.example.demopjgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPjglApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("HolaMundo");
+		SpringApplication.run(DemoPjglApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demopjgl/DemoPjglApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demopjgl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPjglApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
