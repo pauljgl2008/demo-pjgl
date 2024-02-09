4900fe2646f8920dc0a93272b221086a5ec01453
Paúl Jheferson Guevara Linares <pguevarl@emeal.nttdata.com>
2024-02-08 20:30:25 -0500

    feat authorizeHttpRequests by HttpMethod POST, GET with role ADMIN

--- src/main/java/com/example/demopjgl/DemoPjglApplication.java
+++ src/main/java/com/example/demopjgl/DemoPjglApplication.java
@@ -2,8 +2,9 @@ package com.example.demopjgl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class DemoPjglApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/demopjgl/config/SecurityConfiguration.java
+++ src/main/java/com/example/demopjgl/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.example.demopjgl.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,11 +12,36 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    private static final String[] NO_AUTH_LIST ={
+            "/api-docs",
+            "/configuracion/v1",
+            "/swagger-resources",
+            "/configuration/security",
+            "/login"
+    };
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .httpBasic(withDefaults());
-        return http.build();
+        /*
+        return http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults())
+                .formLogin(withDefaults())
+                .build();
+        */
+        return http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(NO_AUTH_LIST).permitAll()
+                        .requestMatchers(HttpMethod.POST, "/*usuarios*/**").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/*usuarios*/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults())
+                .formLogin(withDefaults())
+                .build();
+
     }
 
 
